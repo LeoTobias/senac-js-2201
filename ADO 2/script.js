890d65587761ea16d16ce18fd409a6b8338b1289
unknown <tobiasleonardo7@gmail.com>
2022-05-13 12:46:41 -0300

    Finalizado a parte funcional da ADO2, faltando agora estilizar

--- ADO 2/script.js
+++ ADO 2/script.js
@@ -1,3 +1,24 @@
-function carregaDado(){
-    
+let resultado = document.getElementById('resultado');
+document.querySelector('button#fetch').addEventListener('click', carregaConteudo);
+
+function carregaConteudo(){
+    let nome = document.getElementById('nome').value;
+
+    fetch('https://api.agify.io?name=' + nome,{method:'GET'})
+        .then(function(response){return response.json();})
+        .then(data => {
+            if(nome != ''){
+                resultado.innerHTML = 'Eu acho que sua idade é: ' + data.age;
+                resultado.style.display = 'block';
+                console.log(data.age);
+            }else{
+                resultado.style.display = 'none';
+            }
+    })
+    .catch(erro => console.log(erro));
 }
+
+
+
+
+
